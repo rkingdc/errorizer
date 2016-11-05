bb4feba9736eb0b3cc49c92a60ffd5fc40308d2b
Ian Kyle <iankyle1@gmail.com>
2016-11-05 12:11:42 -0500

    fixed some stupid errors

--- R/errorize.R
+++ R/errorize.R
@@ -1,40 +1,39 @@
 #' @export
-errorize <- function(FUN, stopOnError=TRUE, onErrorReturn=NULL){
-  fname  <- deparse(substitute(FUN))
-  fname2 <- gsub('^.*::', '', fname)
+errorize <- function(FUN, stopOnError=TRUE, onErrorReturn=NULL, ...){
+  fname2 <- gsub('^.*::', '', deparse(substitute(FUN)))
 
   FUN2 <- function() {
     args <- lapply(as.list(match.call())[-1L], eval, parent.frame())
     tryCatch(do.call(FUN, args),
              error = function(e){
                stime <- Sys.time()
-               fname <- sprintf('./%s_error_%.f.Rds', fname2,  stime)
+               flname <- sprintf('./%s_error_%.f.Rds', fname2,  as.numeric(stime))
                saveRDS(object = list(error   = as.character(e),
                                      time    = stime,
                                      fxn     = FUN,
-                                     fxnName = fname,
                                      arglst  = args),
-                       file = fname)
+                       file = flname,
+                       ...)
                if (stopOnError) {
                  stop(sprintf('Wrote to %s on catching "%s"',
-                              fname, as.character(e)))
+                              flname, as.character(e)))
                } else {
-                 warning(sprintf('Wrote to ./%s_error_%.f7.Rds on catching "%s"',
-                              Sys.time(), as.character(e)))
+                 warning(sprintf('Wrote to %s on catching "%s"',
+                              flname, as.character(e)))
                  return(onErrorReturn)
                }
              },
              warning = function(w){
                stime <- Sys.time()
-               fname <- sprintf('./%s_warning_%.f.Rds', fname2, stime)
+               flname <- sprintf('./%s_warning_%.f.Rds', fname2, as.numeric(stime))
                saveRDS(object = list(warning = as.character(w),
                                      time    = stime,
                                      fxn     = FUN,
-                                     fxnName = fname,
                                      arglst  = args),
-                       file = fname)
+                       file = flname,
+                       ...)
                warning(sprintf('Wrote to %s on catching "%s"',
-                               Sys.time(), as.character(w)))
+                               flname, as.character(w)))
              })
   }
   formals(FUN2) <- formals(FUN)

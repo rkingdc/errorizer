5dd4ba76d0409dc890e30759f506d1524f7fa31e
Ian Kyle <iankyle1@gmail.com>
2016-11-06 09:55:07 -0600

    add additional argument fileSuffix

--- R/errorize.R
+++ R/errorize.R
@@ -1,13 +1,27 @@
 #' @export
-errorize <- function(FUN, stopOnError=TRUE, onErrorReturn=NULL, ...){
-  fname2 <- gsub('^.*::', '', deparse(substitute(FUN)))
+errorize <- function(FUN, fileSuffix=NULL, stopOnError=TRUE, onErrorReturn=NULL, ...){
+  fxname <- gsub('^.*::', '', deparse(substitute(FUN)))
+
+  if (length(fileSuffix) > 1) {
+    fileSuffix <- fileSuffix[1]
+    warning("argument fileSuffix should be a vector of length 1. Only the first element will be used")
+  }
+
+  fs <- if (is.null(fileSuffix)) {
+    paste0('_', as.character(as.integer(Sys.time())))
+  } else if (is.na(fileSuffix)) {
+    ''
+  } else {
+    as.character(fileSuffix[1])
+  }
+
 
   FUN2 <- function() {
     args <- lapply(as.list(match.call())[-1L], eval, parent.frame())
     tryCatch(do.call(FUN, args),
              error = function(e){
                stime <- Sys.time()
-               flname <- sprintf('./%s_error_%.f.Rds', fname2,  as.numeric(stime))
+               flname <- sprintf('./%s_error%s.Rds', fxname,  fs)
                saveRDS(object = list(error   = as.character(e),
                                      time    = stime,
                                      fxn     = FUN,
@@ -25,7 +39,7 @@ errorize <- function(FUN, stopOnError=TRUE, onErrorReturn=NULL, ...){
              },
              warning = function(w){
                stime <- Sys.time()
-               flname <- sprintf('./%s_warning_%.f.Rds', fname2, as.numeric(stime))
+               flname <- sprintf('./%s_warning%s.Rds', fxname, fs)
                saveRDS(object = list(warning = as.character(w),
                                      time    = stime,
                                      fxn     = FUN,
